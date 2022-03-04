e52b8837e535fb6ceccb3d6adc62d7514ee7d41b
Hasie200 <hasieuno@gmail.com>
2022-03-04 00:37:30 -0400

    Login and Sign Up Pages

--- build-team/src/pages/Login.jsx
+++ build-team/src/pages/Login.jsx
@@ -1,7 +1,50 @@
 const Login = () => {
-    return ( 
-        <h1>Login</h1>
+    return (
+
+        <div style={{color: "#fff", 
+        background: "#63738a", 
+        backgroundImage: `url('${require("../images/communication.jpg")}')`,
+        fontFamily: `'Roboto', sans-serif` }}>
+    
+    <div style={{ height:"1000px", width: "510px", marginTop: "30px", marginLeft: "25em", marginRight: "auto", marginBottom: "0px", padding:"0px" }}>
+       
+      
+        
+        <br/>
+        <br/>
+        
+    <form id = "form" style={{marginTop: "30px", height: "700px", color: "#999", borderRadius: "3px",marginBottom: "15px", background: "#fff",  boxShadow: "0px 2px 2px rgba(0, 0, 0, 0.3)", padding: "30px"}}>
+       
+        <img src= {require ("../images/logo.png")} style={{paddingLeft: "130px"}}/>
+        
+    
+    
+                                                {/* EMAIL*/}
+    
+    <div>
+    <input type="email" name = "email" placeholder = "Email" id="Email"  style={{height: "41px", width:"450px", background: "#f2f2f2", boxShadow: "none !important", border: "none", borderRadius: "3px", marginBottom: "20px", paddingRight: "10px"}} minlength="8" required />
+    </div>
+                                           {/* PASSWORD */}
+    <div>
+    
+    <input type="password"  name = "password" placeholder = "Password" id="passwrd" style={{height: "41px",  width:"450px", background: "#f2f2f2", boxShadow: "none !important", border: "none", borderRadius: "3px", marginBottom: "20px"}} minlength = "8" required/>
+    <small>Must have at least 8 characters</small>
+    </div>
+    
+    <button type="submit" className="btn btn-primary" style={{ marginTop: "4em", marginLeft: "186px", marginRight: "auto", marginBottom: "auto" , width: "100px", height: "40px"}}>Login</button>
+    
+    </form>
+    
+    <div style={{paddingBottom: "15px" , textAlign: "center"}}>Have you registered with us as yet ?  <a href="http://localhost:3000/signup" style={{color: "white"}}> Sign Up Now </a></div>
+     
+    
+    </div>
+    
+    
+    </div>  
+    
+    
     );
 }
- 
+
 export default Login;
