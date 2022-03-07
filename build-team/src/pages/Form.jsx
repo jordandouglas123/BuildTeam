39b16738e4089d919336ed7af838daf9cc900ede
BigDogJ <31512614+BigDogJ@users.noreply.github.com>
2022-03-07 17:53:09 -0400

    General Update
    Just updating the main the lastest

--- build-team/src/pages/Form.jsx
+++ build-team/src/pages/Form.jsx
@@ -1,62 +1,73 @@
+import { Link } from "react-router-dom";
 const Form = () => {
-  return (
-    <div>
-      <div align="center">
-        <img src="logo.png" alt="" />
-      </div>
-      <h1 align="center">Project Requirements Form </h1>
-      <div class="card w-75 m-auto p-2">
-        <form>
-          <div >
-            <label className="d-block text-center" >
-              Project Description (Give a brief description of the project you
-              intend to execute.)
-            </label>
-            <textarea
-              className="form-control"
-              name=""
-              id=""
-              cols="30"
-              rows="10"
-            ></textarea>
-            <br />
-            <label className="d-block text-center"  htmlFor="">Project Budget</label>
-            <input
-              type="number"
-              data-type="currency"
-              placeholder="$100,000"
-              className="form-control"
-            />
-
-            <br />
-            <label className="d-block text-center"  htmlFor="">
-              Desired team size (Specify the number of members you think your
-              project would require.)
-            </label>
-            <input className="d-block text-center"  type="number" className="form-control" />
-            <br />
-            <label className="d-block text-center"  htmlFor="">
-              Choose desired members (Choose the type of members you would like
-              team to consist of. ){" "}
-            </label>
-            <select className="form-control" name="" id="">
-              <option value="">Project Manager</option>
-              <option value="">System Analyst </option>
-              <option value="">Intern</option>
-              <option value="">Database Administrator</option>
-              <option value="">Software Developer</option>
-            </select>
-            <br />
+    return (
+        <div>
             <div align="center">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>{" "}
+                <img src={require("../images/logo.PNG")} alt="" />
             </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+            <h1 align="center">Project Requirements Form </h1>
+            <div class="card w-75 mx-auto p-4 my-4">
+                <form>
+                    <div>
+                        <label className="d-block text-center">
+                            Project Description (Give a brief description of the
+                            project you intend to execute.)
+                        </label>
+                        <textarea
+                            className="form-control"
+                            name=""
+                            id=""
+                            cols="30"
+                            rows="10"
+                        ></textarea>
+                        <br />
+                        <label className="d-block text-center" htmlFor="">
+                            Project Budget
+                        </label>
+                        <input
+                            type="number"
+                            data-type="currency"
+                            placeholder="$100,000"
+                            className="form-control"
+                        />
+
+                        <br />
+                        <label className="d-block text-center" htmlFor="">
+                            Desired team size (Specify the number of members you
+                            think your project would require.)
+                        </label>
+                        <input
+                            className="d-block text-center"
+                            type="number"
+                            className="form-control"
+                        />
+                        <br />
+                        <label className="d-block text-center" htmlFor="">
+                            Choose desired members (Choose the type of members
+                            you would like team to consist of. ){" "}
+                        </label>
+                        <select className="form-control" name="" id="">
+                            <option value="">Project Manager</option>
+                            <option value="">System Analyst </option>
+                            <option value="">Intern</option>
+                            <option value="">Database Administrator</option>
+                            <option value="">Software Developer</option>
+                        </select>
+                        <br />
+                        <div align="center">
+                            <Link
+                                to="/employer"
+                                type="button"
+                                className="btn btn-primary"
+                            >
+                                Submit
+                            </Link>{" "}
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Form;
--- build-team/src/pages/Home.jsx
+++ build-team/src/pages/Home.jsx
@@ -1,126 +1,181 @@
+import { Link } from "react-router-dom";
 const Home = () => {
-  return (
-    <div>
-      <nav id="home_nav" className="navbar navbar-light  ">
-        <a className="navbar-brand" href="#">
-          <img id="logo" src={require("../images/logo.PNG")} alt="" />
-        </a>
-      </nav>
+    return (
+        <div>
+            <nav
+                id="home_nav"
+                className="navbar navbar-light d-flex justify-content-center"
+            >
+                <a className="navbar-brand" href="#">
+                    <img id="logo" src={require("../images/logo.PNG")} alt="" />
+                </a>
+            </nav>
 
-      <div id="jumbotron" className="jumbotron jumbotron-fluid text-center ">
-        <div className="container">
-          <h1 id="jumboTitle" className="display-4-header">
-            Welcome to BuildTeam
-          </h1>
-        </div>
-        <div className="container d-flex justify-content-evenly" id="buttons">
-          <button
-            id="signUp"
-            type="button"
-            className="btn btn-outline-light btn-lg"
-          >
-            {" "}
-            Sign Up
-          </button>
-          <button type="button" className="btn btn-outline-light btn-lg">
-            {" "}
-            Employer Login
-          </button>
-          <button type="button" className="btn btn-outline-light btn-lg">
-            {" "}
-            Employee Login
-          </button>
-          <button type="button" className="btn btn-outline-light btn-lg">
-            Learn More
-          </button>
-        </div>
-      </div>
-      <div className=" container" id="cardsContainer">
-        <div className="row align-items-center">
-          <div className="col">
-            <div className="card text-center" id="card">
-              <img className=" card-img-top" src="team.jpg" alt="" />
-              <div className="card-body">
-                <h5 className="card-title" id="cardText">
-                  Reliable Project Team Generetion
-                </h5>
-              </div>
+            <div
+                id="jumbotron"
+                className="jumbotron jumbotron-fluid text-center "
+            >
+                <div className="container">
+                    <h1 id="jumboTitle" className="display-4-header">
+                        Welcome to BuildTeam
+                    </h1>
+                </div>
+                <div
+                    className="container d-flex justify-content-evenly"
+                    id="buttons"
+                >
+                    <Link
+                        to="/signup"
+                        id="signUp"
+                        type="button"
+                        className="btn btn-outline-light btn-lg"
+                    >
+                        {" "}
+                        Sign Up
+                    </Link>
+                    <Link
+                        to="/login"
+                        type="button"
+                        className="btn btn-outline-light btn-lg"
+                    >
+                        {" "}
+                        Employer Login
+                    </Link>
+                    <Link
+                        to="/login"
+                        type="button"
+                        className="btn btn-outline-light btn-lg"
+                    >
+                        {" "}
+                        Employee Login
+                    </Link>
+                    <button
+                        type="button"
+                        className="btn btn-outline-light btn-lg"
+                    >
+                        Learn More
+                    </button>
+                </div>
             </div>
-          </div>
+            <div className=" container" id="cardsContainer">
+                <div className="row align-items-center">
+                    <div className="col">
+                        <div className="card text-center" id="card">
+                            <img
+                                className=" card-img-top"
+                                src={require("../images/team.jpg")}
+                                alt=""
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title" id="cardText">
+                                    Reliable Project Team Generetion
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
 
-          <div className="col">
-            <div className="card text-center" id="card">
-              <img className=" card-img-top" src="fast.jpg" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">Fast and Efficient</h5>
-              </div>
-            </div>
-          </div>
+                    <div className="col">
+                        <div className="card text-center" id="card">
+                            <img
+                                className=" card-img-top"
+                                src={require("../images/fast.jpg")}
+                                alt=""
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    Fast and Efficient
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
 
-          <div className="col">
-            <div className="card text-center" id="card">
-              <img className=" card-img-top" src="user2.jpg" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">Easy to Use User Interface</h5>
-              </div>
+                    <div className="col">
+                        <div className="card text-center" id="card">
+                            <img
+                                className=" card-img-top"
+                                src={require("../images/user2.jpg")}
+                                alt=""
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    Easy to Use User Interface
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div className="container">
+                <div className="card text-center-justify">
+                    <h1 className="h1 card-header text-center">
+                        What is BuildTeam?
+                    </h1>
+                    <p className="card-body text-center">
+                        This application generates a project team based on some
+                        provided criteria( skills, qualifications, experience,
+                        budget, salary demands etc..). This application would
+                        compile the information given and generate project
+                        groups based on the users CV’s . This application will
+                        allow a chat box for the users to get to know each other
+                        and ask relevant questions to each member. The business
+                        personnel would open the app, enter their login details,
+                        then they would find a team via findteam button, a form
+                        would be displayed on the screen and they would enter
+                        their requirements for the project(budget, project
+                        description, then when the form is submitted the app
+                        will generate a team/ a list of teams based on the
+                        criteria provided in the form and availability of the
+                        registered IT personnell. Qualified IT personnel can
+                        also register on the application. During the
+                        registration process, they are required to provide some
+                        personal information (name, DOB, etc) and their CV’s.{" "}
+                    </p>
+                </div>
+            </div>
+            <br />
+            <div className="container">
+                <h1 className="container-header text-center">Meet The Team</h1>
+                <div className="row">
+                    <div className="col">
+                        <div className="card text-center">
+                            <img
+                                className=" card-img-top"
+                                src={require("../images/team.jpg")}
+                                alt=""
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">Jael Romain</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card text-center">
+                            <img
+                                className=" card-img-top"
+                                src={require("../images/team.jpg")}
+                                alt=""
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">Hasie Alexander</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card text-center">
+                            <img
+                                className=" card-img-top"
+                                src={require("../images/team.jpg")}
+                                alt=""
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">Jordan Douglas</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="container">
-        <div className="card text-center-justify">
-          <h1 className="h1 card-header text-center">What is BuildTeam?</h1>
-          <p className="card-body ">
-            This application generates a project team based on some provided
-            criteria( skills, qualifications, experience, budget, salary demands
-            etc..). This application would compile the information given and
-            generate project groups based on the users CV’s . This application
-            will allow a chat box for the users to get to know each other and
-            ask relevant questions to each member. The business personnel would
-            open the app, enter their login details, then they would find a team
-            via findteam button, a form would be displayed on the screen and
-            they would enter their requirements for the project(budget, project
-            description, then when the form is submitted the app will generate a
-            team/ a list of teams based on the criteria provided in the form and
-            availability of the registered IT personnell. Qualified IT personnel
-            can also register on the application. During the registration
-            process, they are required to provide some personal information
-            (name, DOB, etc) and their CV’s.{" "}
-          </p>
-        </div>
-      </div>
-      <br />
-      <div className="container">
-        <h1 className="container-header text-center">Meet The Team</h1>
-        <div className="row">
-          <div className="col">
-            <div className="card text-center">
-              <img className=" card-img-top" src="team.jpg" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">Jael Romain</h5>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card text-center">
-              <img className=" card-img-top" src="team.jpg" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">Hasie Alexander</h5>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card text-center">
-              <img className=" card-img-top" src="team.jpg" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">Jordan Douglas</h5>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
