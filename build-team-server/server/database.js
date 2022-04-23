82b1957b3f3090fdea67b5e5358c9d098c11646f
jordandouglas120 <jordandouglas120@gmail.com>
2022-04-23 16:57:33 -0400

    algorithms update

--- build-team-server/server/database.js
+++ build-team-server/server/database.js
@@ -1,6 +1,6 @@
-module.exports = async () => {
-    const mysql = require('promise-mysql2');
-    let connection = await mysql.createConnection({
+module.exports = () => {
+    const mysql = require('mysql');
+    let connection = mysql.createConnection({
         host: "us-cdbr-east-05.cleardb.net",
         user: "b66fc3ccca05d0",
         password: "ea3b0f54",
